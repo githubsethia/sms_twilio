61524638a52d53b082f449d2e45a8023ad151f61
Manish <manish.sethia@gmail.com>
2020-04-01 20:05:12 +0800

    revised version

---
+++ sms_twilio.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'SSSSSSSSSSS'
+auth_token = 'SSSSSSSSSSS'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         body='This is the ship that made the Kessel Run in fourteen parsecs?',
+         from_='+14692083453',
+         to='+6591257651'
+     )
+
+print(message.sid)
